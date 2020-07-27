be74af5acfcb50470e1468221281501d4b09ff45
usherxd <usherxd@gmail.com>
2020-07-27 12:16:33 +0800

    USA and else part

--- Driving.py
+++ Driving.py
@@ -2,8 +2,15 @@ country = input('請問您是那國人： ')
 age = input('請問您的年齡： ')
 age = int(age)
 
-if country == '台灣' or 'taiwan':
+if country == '台灣':
     if age >= 18:
-        print('您', age ,'歲，可以考駕照哦！')
+        print('您是', country, ',', age ,'歲，可以考駕照哦！')
     else:
         print('您未滿18歲，騎單車吧！')
+elif country == '美國':
+    if age >= 16:
+        print('您是', country, ',', age ,'歲，可以考駕照哦！')
+    else:
+        print('您未滿16歲，騎單車吧！')      
+else:
+    print('目前僅能輸入「台灣」與「美國」哦！') 
